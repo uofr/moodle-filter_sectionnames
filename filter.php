5da927380ec810c0b0839f2e180af63f2468094b
Matt Davidson <davidso1@rose-hulman.edu>
2017-03-13 10:49:22 -0400

    Added short filter summary for phpdoc support

--- filter.php
+++ filter.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Automatically links section names in a Moodle course and its activities
+ * 
  * This filter provides automatic linking to sections when its name (title)
  * is found inside every Moodle text
  *
