4312559a093dab9b38f5f9b69cddfa4f8ac778f9
Matt Davidson <davidso1@rose-hulman.edu>
2017-03-13 09:34:00 -0400

    Start point
    Created from the activitynames filter.  Altered to only process section
    names.

---
+++ filter.php
@@ -0,0 +1,122 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This filter provides automatic linking to
+ * sections when its name (title) is found inside every Moodle text
+ *
+ * @package    filter
+ * @subpackage sectionnames
+ * @copyright  2017 Matt Davidson
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Section name filtering
+ */
+class filter_sectionnames extends moodle_text_filter {
+    // Trivial-cache - keyed on $cachedcourseid and $cacheduserid.
+    static $sectionslist = null;
+    static $cachedcourseid;
+    static $cacheduserid;
+
+    function filter($text, array $options = array()) {
+        global $USER; // Since 2.7 we can finally start using globals in filters.
+
+        $coursectx = $this->context->get_course_context(false);
+        if (!$coursectx) {
+            return $text;
+        }
+        $courseid = $coursectx->instanceid;
+
+        // Initialise/invalidate our trivial cache if dealing with a different course.
+        if (!isset(self::$cachedcourseid) || self::$cachedcourseid !== (int)$courseid) {
+            self::$sectionslist = null;
+        }
+        self::$cachedcourseid = (int)$courseid;
+        // And the same for user id.
+        if (!isset(self::$cacheduserid) || self::$cacheduserid !== (int)$USER->id) {
+            self::$sectionslist = null;
+        }
+        self::$cacheduserid = (int)$USER->id;
+
+        /// It may be cached
+
+        if (is_null(self::$sectionslist)) {
+            self::$sectionslist = array();
+
+            $modinfo = get_fast_modinfo($courseid);
+            self::$sectionslist = array(); // We will store all the created filters here.
+
+            // Create array of visible sections sorted by the name length (we are only interested in properties name and url).
+            $sortedsections = array();
+
+            $numsections = course_get_format($courseid)->get_course()->numsections;
+            $section = 1; // Skip the general section 0
+            while ($section <= $numsections) {
+                if ($modinfo->get_section_info($section)->visible) {
+                    $sortedsections[] = (object)array(
+                        'name' => get_section_name($courseid, $section),
+                        'url' => course_get_url($courseid, $section),
+                        'id' => $section,
+                        'namelen' => -strlen(get_section_name($courseid, $section)), // Negative value for reverse sorting.
+                    );
+                }
+                $section++;
+            }
+
+            // Sort activities by the length of the section name in reverse order.
+            core_collator::asort_objects_by_property($sortedsections, 'namelen', core_collator::SORT_NUMERIC);
+
+            foreach ($sortedsections as $section) {
+                $title = s(trim(strip_tags($section->name)));
+                $currentname = trim($section->name);
+                $entitisedname  = s($currentname);
+                // Avoid empty or unlinkable activity names.
+                if (!empty($title)) {
+                    $href_tag_begin = html_writer::start_tag('a',
+                            array('class' => 'autolink', 'title' => $title,
+                                'href' => $section->url));
+                    self::$sectionslist[$section->id] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
+                    if ($currentname != $entitisedname) {
+                        // If name has some entity (&amp; &quot; &lt; &gt;) add that filter too. MDL-17545.
+                        self::$sectionslist[$section->id.'-e'] = new filterobject($entitisedname, $href_tag_begin, '</a>', false, true);
+                    }
+                }
+            }
+        }
+
+        $filterslist = array();
+        if (self::$sectionslist) {
+            $sectionid = $this->context->instanceid;
+            if ($this->context->contextlevel == CONTEXT_MODULE && isset(self::$sectionslist[$sectionid])) {
+                // remove filterobjects for the current module
+                $filterslist = array_values(array_diff_key(self::$sectionslist, array($sectionid => 1, $sectionid.'-e' => 1)));
+            } else {
+                $filterslist = array_values(self::$sectionslist);
+            }
+        }
+
+        if ($filterslist) {
+            return $text = filter_phrases($text, $filterslist);
+        } else {
+            return $text;
+        }
+    }
+}
