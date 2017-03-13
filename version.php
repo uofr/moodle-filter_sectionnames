bf8e9d084af8c4e1ed4120c40e90613ccd4e01f5
Matt Davidson <davidso1@rose-hulman.edu>
2017-03-13 09:42:55 -0400

    Release 3.2

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2017030800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'filter_sectionnames'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '3.2';
